d7ecf189aafd467e99c3acc82494d4d894f23ae0
Sneha <sneha@gmail.com>
2026-04-08 09:44:36 +0000

    Update server_stats.sh to write performance stats to README.md

--- Server_Performance_Stats/README.md
+++ Server_Performance_Stats/README.md
@@ -1,21 +1,25 @@
-Current date and time is : Wed Apr  8 09:35:55 UTC 2026
-Total CPU Usage is : %Cpu(s):  0.0 us,  6.1 sy,  0.0 ni, 93.9 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
-Total Memory Usage is : Free: 7.73423%
- Used: 52.77%
+###Server Performance Stats
+
+'''
+Current date and time is : Wed Apr  8 09:44:23 UTC 2026
+Total CPU Usage is : %Cpu(s):  3.2 us, 16.1 sy,  0.0 ni, 80.6 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+Total Memory Usage is : Free: 3.85235%
+ Used: 56.7075%
 Total Disk Usage is : Used: 8.1G | Free: 16G
 Top 5 processes by CPU usage are :     PID    PPID CMD                         %CPU
-   4180    4093 /home/cloud_user/.vscode-se  4.9
-   4093    4089 /home/cloud_user/.vscode-se  1.5
-   4042    3998 /home/cloud_user/.vscode-se  1.0
-   4191    4093 /home/cloud_user/.vscode-se  0.3
+   4180    4093 /home/cloud_user/.vscode-se  6.0
+   4093    4089 /home/cloud_user/.vscode-se  1.0
+   4042    3998 /home/cloud_user/.vscode-se  0.8
       1       0 /sbin/init                   0.2
+    538       1 /usr/lib/snapd/snapd         0.2
 Top 5 processes by memory usage are :     PID    PPID CMD                         %MEM
-   4180    4093 /home/cloud_user/.vscode-se 31.4
+   4180    4093 /home/cloud_user/.vscode-se 36.8
    4093    4089 /home/cloud_user/.vscode-se  7.6
-   4141    4093 /home/cloud_user/.vscode-se  3.7
+   4141    4093 /home/cloud_user/.vscode-se  3.8
    4374    4180 /home/cloud_user/.vscode-se  3.3
    4191    4093 /home/cloud_user/.vscode-se  3.2
-Uptime is : up 58 minutes
+Uptime is : up 1 hour, 6 minutes
 OS version is : Ubuntu 20.04.6 LTS
 Logged in users are : cloud_user pts/0        2026-04-08 08:43 (localhost)
 cloud_user pts/1        2026-04-08 09:17 (localhost)
+'''
--- Server_Performance_Stats/server_stats.sh
+++ Server_Performance_Stats/server_stats.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+FILE="README.md"
+
+echo -e "###Server Performance Stats\n" >> $FILE
+
+echo "'''" >> $FILE
+
 #Date
-echo "Current date and time is : $(date)" >> README.md
+echo "Current date and time is : $(date)" >> $FILE
 #Total CPU Usage is
-echo "Total CPU Usage is : $(top -bn1 | grep "Cpu(s)")" >> README.md
+echo "Total CPU Usage is : $(top -bn1 | grep "Cpu(s)")" >> $FILE
 #Total memory usage (Free vs Used including percentage)
-echo "Total Memory Usage is : $(free | grep Mem | awk '{print "Free: " $4/$2 * 100 "%\n", "Used: " $3/$2 * 100 "%"}')" >> README.md
+echo "Total Memory Usage is : $(free | grep Mem | awk '{print "Free: " $4/$2 * 100 "%\n", "Used: " $3/$2 * 100 "%"}')" >> $FILE
 #Total disk usage (Free vs Used including percentage)
-echo "Total Disk Usage is : $(df -h --total | grep total | awk '{print "Used: " $3 " | Free: " $4 }')" >> README.md
+echo "Total Disk Usage is : $(df -h --total | grep total | awk '{print "Used: " $3 " | Free: " $4 }')" >> $FILE
 #Top 5 processes by CPU usage
-echo "Top 5 processes by CPU usage are : $(ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6 )" >> README.md
+echo "Top 5 processes by CPU usage are : $(ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6 )" >> $FILE
 #Top 5 processes by memory usage
-echo "Top 5 processes by memory usage are : $(ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6 )" >> README.md
+echo "Top 5 processes by memory usage are : $(ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6 )" >> $FILE
 #uptime is
-echo "Uptime is : $(uptime -p)" >> README.md
+echo "Uptime is : $(uptime -p)" >> $FILE
 #OS version
-echo "OS version is : $(lsb_release -d | awk -F"\t" '{print $2}')" >> README.md
+echo "OS version is : $(lsb_release -d | awk -F"\t" '{print $2}')" >> $FILE
 #login users
-echo "Logged in users are : $(who)" >> README.md
+echo "Logged in users are : $(who)" >> $FILE
+
+echo "'''" >> $FILE
